29c0b7cda9ea66c3f8583a95593686d5ef75eb51
amataku <amataku13@gmail.com>
2018-05-17 14:17:13 +0900

    switch:function

--- cost.erl
+++ cost.erl
@@ -9,13 +9,6 @@ main(A,S) ->
   Shalf=sigma(A,S),
   Fhalf / Shalf.
 
-%% 階乗計算
-factorial(0) ->
-  1;
-
-factorial(N) ->
-  N * factorial(N - 1).
-
 %% 分母の計算
 sigma(A,0) ->
   0;
@@ -24,6 +17,13 @@ sigma(A,S) ->
   T=power(A,S),
   F=factorial(S),
   (T / F) + sigma(A,S - 1).
+
+%% 階乗計算
+factorial(0) ->
+  1;
+
+factorial(N) ->
+  N * factorial(N - 1).
 
 %% 累乗計算
 power(N,M) ->
