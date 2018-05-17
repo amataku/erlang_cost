725d7cde9db2b4078ebd5569d3ef75b1e6515cbc
amataku <amataku13@gmail.com>
2018-05-17 14:14:42 +0900

    make:cost system

--- cost.erl
+++ cost.erl
@@ -1,11 +1,30 @@
 -module(cost).
--export([main/1]).
+-export([main/2]).
 
-main(Arg) ->
-  factorial(Arg).
+%% メイン関数
+main(A,S) ->
+  Top=power(A,S),
+  Second=factorial(S),
+  Fhalf=Top / Second,
+  Shalf=sigma(A,S),
+  Fhalf / Shalf.
 
+%% 階乗計算
 factorial(0) ->
   1;
-  
+
 factorial(N) ->
   N * factorial(N - 1).
+
+%% 分母の計算
+sigma(A,0) ->
+  0;
+
+sigma(A,S) ->
+  T=power(A,S),
+  F=factorial(S),
+  (T / F) + sigma(A,S - 1).
+
+%% 累乗計算
+power(N,M) ->
+  math:pow(N,M).
